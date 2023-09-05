06ee82419cc517b84f98dc516735dccc5e94985b
Kristian <appstreams@AppStreamss-MacBook-Pro.local>
2023-09-05 12:04:19 +0300

    refactor(controllers): created friends controller to separate logic

---
+++ src/server/controllers/FriendsController.js
@@ -0,0 +1,27 @@
+import { ADDED_FRIEND, REMOVED_FRIEND } from '../../constants/messages.js';
+
+export class FriendsController {
+  constructor(service) {
+    this.service = service;
+  }
+
+  async addFriend(req, res) {
+    const { id } = req.user;
+    const { friendUsername } = req.body;
+
+    await this.service.addFriend(id, friendUsername);
+    res.status(200).json({
+      message: ADDED_FRIEND
+    });
+  }
+
+  async removeFriend(req, res) {
+    const { id } = req.user;
+    const { friendUsername } = req.body;
+
+    await this.service.removeFriend(id, friendUsername);
+    res.status(200).json({
+      message: REMOVED_FRIEND
+    });
+  }
+}
